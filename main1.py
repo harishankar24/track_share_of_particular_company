53a8d90ba8deac42dd151d0c7e9f0e09461d1541
Hari <hari@shankar.com>
2021-01-27 13:07:09 +0530

    top 4 news working

--- main1.py
+++ main1.py
@@ -1,22 +1,53 @@
-from confidential_data import stock_api_key
+#Resource1-See share-market information here: https://www.tradingview.com/markets/stocks-usa/market-movers-large-cap/
+#Resource2-https://www.alphavantage.co/
+#Resource3-https://newsapi.org/
+
+from confidential_data import stock_api_key,news_api_key
 import requests
 
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
-
-STOCK_ENDPOINT = "https://www.alphavantage.co/query"
-NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+company_code = "KOSS"
+company_name = "KOSS CORP"
+stock_api = "https://www.alphavantage.co/query"
+news_api = "https://newsapi.org/v2/everything"
 
 stock_api_parameter = {
     "function": "TIME_SERIES_DAILY",
-    "symbol": "TSLA",
+    "symbol": company_code,
     "apikey": stock_api_key
 }
-response = requests.get(url = STOCK_ENDPOINT, params = stock_api_parameter)
+response = requests.get(url = stock_api, params = stock_api_parameter)
 response.raise_for_status()
-data = response.json()
-value1 = float(data["Time Series (Daily)"]["2021-01-25"]["4. close"])
-value2 = float(data["Time Series (Daily)"]["2021-01-22"]["4. close"])
-diff = abs(value1 - value2)
-diff_in_percent = int((diff * 100)/value2)
-print(diff_in_percent)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key,value) in data.items()]
+
+yesterday_closing_price = float(data_list[0]["4. close"])
+day_before_yesterday_closing_price = float(data_list[1]["4. close"])
+difference = (yesterday_closing_price - day_before_yesterday_closing_price)
+difference_in_percent = round((difference * 100)/day_before_yesterday_closing_price,2)
+if(difference_in_percent >= 0):
+    symbol = "🔺"
+else:
+    symbol = "🔻"
+print(f"Share price: {symbol} {abs(difference_in_percent)}%")
+
+if (difference_in_percent > 5):    
+    news_api_parameter = {
+        "q":company_name,
+        "language": "en",
+        "pageSize": 4,
+        "apiKey": news_api_key
+    }
+    top_news = []
+    response = requests.get(url = news_api, params = news_api_parameter)
+    response.raise_for_status()
+    data = response.json()
+    for _ in range(news_api_parameter["pageSize"]):
+        new_dict = dict()
+        new_dict["title"] = data["articles"][_]["title"]
+        new_dict["description"] = data["articles"][_]["description"]
+        top_news.append(new_dict)
+    for each_news in top_news:
+        print(f"Title: {each_news['title']}")
+        print(f"Description: {each_news['description']}")
+        print()
+    
